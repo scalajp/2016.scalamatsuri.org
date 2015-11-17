bf2ba62c0a69748b1eea52e74970129a1cd88072
Eugene Yokota <eed3si9n@gmail.com>
2015-11-17 14:19:57 -0500

    display session description in a modal dialog

---
+++ js/scalamatsuri2016.js
@@ -0,0 +1,70 @@
+var scalamatsuri = scalamatsuri || {};
+
+scalamatsuri.displayDescription = function(id) {
+  $("#dialog_message iframe").remove();
+  $("#dialog_message").append(
+    $("<iframe />").attr("src", "/" + lang + "/candidates/" + id + "/").addClass("schedule_desc")
+  ).dialog({
+    modal: true,
+    width: 800,
+    height: 600,
+    buttons: {
+      Ok: function() {
+        $(this).dialog("close");
+      }
+    }
+  });
+}
+
+$(document).ready(function(){
+  $(".gnavSmClick").click(function(){
+    $(".smVerGnav").slideToggle();
+  });
+
+  $(".btn1space").click(function(){
+    resetTable();
+    $(".roomA").show();
+    $(this).addClass("onNavLink");
+  });
+
+  $(".btn2space").click(function(){
+    resetTable();
+    $(".roomB").show();
+    $(this).addClass("onNavLink");
+  });
+
+  $(".btn3space").click(function(){
+    resetTable();
+    $(".roomC").show();
+    $(this).addClass("onNavLink");
+  });
+
+  function resetTable(){
+    $(".btn1space").removeClass("onNavLink");
+    $(".btn2space").removeClass("onNavLink");
+    $(".btn3space").removeClass("onNavLink");
+    $(".roomA").hide();
+    $(".roomB").hide();
+    $(".roomC").hide();
+  }
+
+  function initSchedule(){
+    if ($(document).width() >= 1024) {
+      $('.day1roomswitch').hide();
+      $('.day2roomswitch').hide();
+      $(".roomA").show();
+      $(".roomB").show();
+      $(".roomC").show();
+    } else {
+      resetTable();
+      $('.day1roomswitch').show();
+      $('.day2roomswitch').show();
+      $(".btn1space").addClass("onNavLink");
+      $(".roomA").show();
+    }
+  }
+  $(window).resize(function() {
+    initSchedule();
+  });
+  initSchedule();
+});
